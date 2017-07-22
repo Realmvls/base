ac285a9b0b5ede241fd1f5efc64752a4e0ff1521
jochen <942814208@qq.com>
2017-07-22 17:57:31 +0800

    pythoy turtle五角星

---
+++ 1.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+import time
+p = Turtle()
+p.speed(3)
+p.pensize(5)
+p.color("black","yellow")
+p.begin_fill()
+for i in range(5):
+    p.forward(200)
+    p.right(144)
+p.end_fill()
+time.sleep(2)
